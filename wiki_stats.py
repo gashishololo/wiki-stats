357d53bea028210bf94cef8cbff2de6600f44e46
Gosha <sarancha.zhora@ya.ru>
2016-05-16 11:02:56 +0400

    Update wiki_stats.py

--- wiki_stats.py
+++ wiki_stats.py
@@ -3,17 +3,14 @@
 import os
 import sys
 import math
-
 import array
 
 import statistics
 
 from matplotlib import rc
-"""
 rc('font', family='Droid Sans', weight='normal', size=14)
-"""
+
 import matplotlib.pyplot as plt
-import matplotlib.mlab as mlab
 
 
 class WikiGraph:
@@ -22,42 +19,46 @@ class WikiGraph:
         print('Загружаю граф из файла: ' + filename)
 
         with open(filename) as f:
-
-            (n, _nlinks) = (map(int, f.readline().split()))
+            (n, _nlinks) = tuple(int(i) for i in f.readline()[:-1].split())
+            self._n, self._nlinks = n, _nlinks
             self._titles = []
-            steps = 2854434 / 6  
             self._sizes = array.array('L', [0]*n)
             self._links = array.array('L', [0]*_nlinks)
             self._redirect = array.array('B', [0]*n)
             self._offset = array.array('L', [0]*(n+1))
-            n_lks = 0 # current number of links
-            for i in range(n):
-                self._titles.append(f.readline().rstrip())
-                (size, redirect, lks) = (map(int, f.readline().split()))
-                self._sizes[i] = size
-                self._redirect[i] = redirect
-                for j in range(n_lks, n_lks + lks):
-                    self._links[j] = int(str(f.readline()))
-                n_lks += lks
-                self._offset[i+1] = self._offset[i] + lks
-                if i % steps == 0:
-                    print ('*', end = ' ')
-            print('Done!')
+
+            number  = 0
+
+            for line in range(n):
+                self._titles.append(f.readline()[:-1])
+                size, flag, links = tuple(int(i) for i in f.readline()[:-1].split())
+                self._sizes[line] = size
+                self._offset[line] = number
+
+                if flag == 1:
+                    self._redirect[line] = True
+                else:
+                    self._redirect[line] = False
+
+
+                for one in range(links):
+                    self._links[number + one] = int(f.readline()[:-1])
+                number += links
+
         print('Граф загружен')
+        f.close()
 
     def get_number_of_links_from(self, _id):
-        return len(self._links[self._offset[_id]:self._offset[_id+1]])
+        return len(self._links[self._offset[_id]:self._offset[_id + 1]])
 
     def get_links_from(self, _id):
         return self._links[self._offset[_id]:self._offset[_id+1]]
 
     def get_id(self, title):
-        for i in range(len(self._titles)):
-            if self._titles[i] == title:
-                return(i)
+        return self._titles.index(title)
 
     def get_number_of_pages(self):
-        return len(self._titles)
+        return self._n
 
     def is_redirect(self, _id):
         return self._redirect[_id]
@@ -66,96 +67,21 @@ class WikiGraph:
         return self._titles[_id]
 
     def get_page_size(self, _id):
-        return self._sizes(_id)
+        return self._sizes[_id]
 
-def analyse_links_from_page(G):
-    numlinks_from = list(map(G.get_number_of_links_from, range(G.get_number_of_pages())))
-    _max = max(numlinks_from)
-    _min = min(numlinks_from)
-    mxn = sum(x == _max for x in numlinks_from)
-    mnn = sum(x == _min for x in numlinks_from)
-    print("Minimal number of links from page:", _min)
-    print("Pages with minimal number of links:", mnn)
-    print("Maximal number of links from page:", _max)
-    print("Pages with maximal number of links:", mxn)
-    for i in range(G.get_number_of_pages()):
-        if G.get_number_of_links_from(i) == _max:
-            break
-    print("Page with maximal number of links:",  G.get_title(i))
-    print("Mean number of links: %0.2f  (st. dev. : %0.2f)" %(statistics.mean(numlinks_from), statistics.stdev(numlinks_from)))
-    hist("lfrom", numlinks_from, 300, 200, "aa", "bb", "zz")
-    
-def analyse_links_to_page(G):
-    numlinks_to = [0 for i in range(G.get_number_of_pages())]
-    for i in range(G.get_number_of_pages()):
-        for x in G.get_links_from(i):
-            numlinks_to[x] += 1
-            if G.is_redirect(i) == 1:
-                numlinks_to[x] -= 1
-    _max = max(numlinks_to)
-    _min = min(numlinks_to)
-    mxn = sum(x == _max for x in numlinks_to)
-    mnn = sum(x == _min for x in numlinks_to)
-    print("Minimal number of links to page:", _min)
-    print("Pages with minimal number of to-links:", mnn)
-    print("Maximal number of links to page:", _max)
-    print("Pages with maximal number of to-links:", mxn)
-    for i in range(G.get_number_of_pages()):
-        if numlinks_to[i] == _max:
-            break
-    print("Page with maximal number of to-links:",  G.get_title(i))
-    print("Mean number of to-links: %0.2f  (st. dev. : %0.2f)" %(statistics.mean(numlinks_to), statistics.stdev(numlinks_to)))
-    
-    hist("linksto", numlinks_to, 300, 150, "aa", "bb", "zz")
-def analyse_redirects(G):
-    redirects_to = [0 for i in range(G.get_number_of_pages())]
-    for i in range(G.get_number_of_pages()):
-        for x in G.get_links_from(i):
-            if G.is_redirect(i) == 1:
-                redirects_to[x] += 1
-    _max = max(redirects_to)
-    _min = min(redirects_to)
-    mxn = sum(x == _max for x in redirects_to)
-    mnn = sum(x == _min for x in redirects_to)
-    print("Minimal number of redirects to page:", _min)
-    print("Pages with minimal number of redirects:", mnn)
-    print("Maximal number of redirects to page:", _max)
-    print("Pages with maximal number of redirects:", mxn)
-    for i in range(G.get_number_of_pages()):
-        if redirects_to[i] == _max:
-            break
-    print("Page with maximal number of redirects:",  G.get_title(i))
-    print("Mean number of redirects: %0.2f  (st. dev. : %0.2f)" %(statistics.mean(redirects_to), statistics.stdev(redirects_to)))
-    number_of_redirects_to = [0 for i in range(_max+2)]
-    print(_max)
-    hist("redir", redirects_to, 100, 50, "aa", "bb", "zz")
-    
-def bfs(G, start, target):
-    path = []
-    queue = [start]
-    while queue:
-        v = queue.pop(0)
-        if v not in path:
-            path.append(v)
-            queue = queue + list(G.get_links_from(v))
-            if v == target:
-                break
-    return path
+    def get_all_links(self):
+        return self._nlinks
 
 
 
-def hist(fname, data, bins, range,  xlabel, ylabel, title, facecolor='green', alpha=0.5, transparent=True, **kwargs):
+
+def hist(fname, data, bins, xlabel, ylabel, title, facecolor='green', alpha=0.5, transparent=True, **kwargs):
     plt.clf()
-    plt.yscale("log")
-    n, bins, patches = plt.hist(data, bins, (0, range), facecolor='green', alpha=0.5)
-    plt.savefig(fname, format="pdf")
-
-def hist_log(fname, data, bins, range,  xlabel, ylabel, title, facecolor='green', alpha=0.5, transparent=True, **kwargs):
-    plt.clf()
-    plt.xscale("log")
-    n, bins, patches = plt.hist(data, bins, (0, range), facecolor='green', alpha=0.5)
-    plt.savefig(fname, format="eps")
-    
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.hist(x=data, bins=bins, facecolor=facecolor, alpha=alpha, **kwargs)
+    plt.savefig(fname, transparent=transparent)
 
 
 if __name__ == '__main__':
@@ -167,16 +93,59 @@ if __name__ == '__main__':
     if os.path.isfile(sys.argv[1]):
         wg = WikiGraph()
         wg.load_from_file(sys.argv[1])
+        maxlinks = max(wg.get_links_from(i) for i in range(wg.get_number_of_pages()))
+        print('Maximum number of links  = ', len(maxlinks))
+        minlinks = min(wg.get_links_from(i) for i in range(wg.get_number_of_pages()))
+        print('Minimum number of links = ', len(minlinks))
+        redirlinks = sum(1 for i in range(wg.get_number_of_pages()) if wg.is_redirect(i))
+        print('The number of links with redirection = ', redirlinks)
+        pageminlink = sum(1 for i in range(wg.get_number_of_pages()) if len(wg.get_links_from(i)) == len(minlinks))
+        print('The number of articles with minlinks = ', pageminlink)
+        pageminlink = sum(1 for i in range(wg.get_number_of_pages()) if len(wg.get_links_from(i)) == len(maxlinks))
+        print('The number of articles with maxlinks = ', pageminlink)
+        averagelink = statistics.mean(wg.get_number_of_links_from(i)for i in range(wg.get_number_of_pages()))
+        print('The AVERAGE number of links = ', round(averagelink,0))
+        links_on=[0]*wg.get_number_of_pages()
+        for u in range(wg.get_number_of_pages()):
+            for t in wg.get_links_from(u):
+                links_on[t]+=1
+        m = min(links_on)
+        n = max(links_on)
+        print('Min links on page = ', m)
+        print('Max links on page =', n)
+        print('Maxlinked page =', wg.get_title(links_on.index(n)))
+
+        def find_links(i):
+            number = 0
+            for t in range(wg.get_number_of_pages()):
+                for z in wg.get_links_from(t):
+                    if z == i:
+                        number += 1
+            return(number)
+
+        number_minlinked = sum(1 for i in range(wg.get_number_of_pages() + 1) if find_links(i) == m)
+        numbermax_linked = sum(1 for i in range(wg.get_number_of_pages() + 1) if find_links(i) == n)
+        dirs = [0] * wg.get_number_of_pages()
+        for i in range(wg.get_number_of_pages()):
+            if not wg.is_redirect(i):
+                for lnk in wg.get_links_from(i):
+                    dirs[lnk] += 1
+        print('Minimum number of redirections to the links', min(dirs))
+        print('Maximum number of redirections to the link', max(dirs))
+        print('Number of links with minimum of redirections', dirs.count(min(dirs)))
+        print('Number of links with maximum of redirections', dirs.count(max(dirs)))
+        print('Number of minlinked pages = ', number_minlinked)
+        print('Number of maxlinked pages = ', numbermax_linked)
+        hist(fname='4-1.png', data=[wg.get_number_of_links_from(i) for i in range(wg._n)], bins=200,xlabel='Количество статей', ylabel="Количество ссылок", title="Распределение количества ссылок из статьи")
+        plt.show()
+        hist(fname='4-2.png', data=[links_on[i] for i in range(wg._n)], bins=50, xlabel='Количество статей',ylabel='Количество ссылок', title='Распределение количества ссылок на статью')
+        plt.show()
+        hist(fname='4-3.png', data=[wg.get_page_size(i) for i in range(wg._n) ], bins=200,xlabel='Количество статей',ylabel='Размер', title='Распределение размеров по статьям')
+        plt.show()
+        hist(fname='4-4.png', data=[math.log(wg.get_page_size(i),math.e) for i in range(wg._n)], bins=200,xlabel='Количество статей',ylabel='Размер', title='Распределение размеров по статьям в логарифмическом масштабе')
+        plt.show()
+        hist(fname='4-5.png', data=[dirs[i] for i in range(wg.get_number_of_pages())], bins=200, xlabel='Количество статей', ylabel='Количество перенаправлений на статью', title='Распределение количества перенаправлений на статью')
+        plt.show()
     else:
         print('Файл с графом не найден')
         sys.exit(-1)
-
-    print("Number of pages with redirect:", sum(wg._redirect))
-    analyse_links_from_page(wg)
-    analyse_links_to_page(wg)
-    analyse_redirects(wg)
-    A = bfs(wg, wg.get_id("Python"), wg.get_id("Список_файловых_систем"))
-    for i in range(len(A)):
-        A[i] = wg.get_title(A[i])
-    hist("size", wg._sizes, 100, 40000, "aa", "bb", "zz")
-    print("Done!")
